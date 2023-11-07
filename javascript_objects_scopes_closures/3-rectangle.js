a301462659619bd43fc129cca01dd85af0e74293
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-11-07 04:13:58 -0800

    4th commit rec 4

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// This script contains a class Rectangle 
+
+class Rectangle {
+    constructor(w, h) {
+    if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+    }   
+        }
+    
+        print() {
+            if (this.width && this.height) {
+                for (let i = 0; i < this.height; i++){
+                    console.log('X'.repeat(this.width));
+                }
+            }
+        }
+}
+
+module.exports = Rectangle;
